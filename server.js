fe5f1db0005846eeff6acf53b97dbe689aedcab2
Jonathan Quiambao <jcquiambao@ucdavis.edu>
2019-09-23 18:44:25 -0700

    add login POST route

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+app.use(bodyParser.json());
 
 const mockUserData = [
     {name:'Mark'},
@@ -14,6 +16,27 @@ app.get('/users/:id', function(req, res) {
         message: 'got one user',
         user: req.params.id
     });
+});
+
+app.post('/login', function(req, res) {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    const mockUsername = "billythekid";
+    const mockPassword = "superSecret";
+
+    if (username === mockUsername && password === mockPassword) {
+        res.json({
+            success: true,
+            message: 'password and username match!',
+            token: 'encrypted token goes here'
+        });
+    } else {
+        res.json({
+            success: false,
+            message: 'password and username don\'t match'
+        });
+    }
 });
 
 app.get('/users', function(req, res) {
